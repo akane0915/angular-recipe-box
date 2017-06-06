af6c79719aa0337b712ff4255b4a696b653765ad
Asia Kane and Susha Dore <susha@>
2017-06-06 11:47:30 -0700

    add update function to specific recipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,8 +6,16 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Granny's Recipes</h1>
     <ul>
-      <li *ngFor="let recipe of recipes">{{recipe.title}}</li>
+      <li *ngFor="let recipe of recipes">{{recipe.title}} <button class="btn" (click)="editRecipe(recipe)">Edit Recipe</button></li>
     </ul>
+
+    <h4>Edit Recipe</h4>
+    <label>Recipe Title:</label>
+    <input [(ngModel)]="selectedRecipe.title">
+    <label>ingredients:</label>
+    <input [(ngModel)]="selectedRecipe.ingredients">
+    <label>Directions:</label>
+    <input [(ngModel)]="selectedRecipe.directions">
   </div>
   `
 })
@@ -16,7 +24,14 @@ export class AppComponent {
   recipes: Recipe[] = [
     new Recipe('Apple Pie', 'Apples, sugar, butter, cinnamon, flour, water', 'Prep apples, prep crust, bake'),
     new Recipe('Liver and Onions', 'Liver, onions', 'cook them and then most likely toss it because it is inedible really'),
+    new Recipe('Plastic Noodles', 'Beef, rice noodles, onions, peppers, tofu, sauce', 'cook it in a wok')
   ];
+
+  selectedRecipe: Recipe = this.recipes[0];
+
+  editRecipe(clickedRecipe) {
+    this.selectedRecipe = clickedRecipe;
+  }
 }
 
 export class Recipe {
