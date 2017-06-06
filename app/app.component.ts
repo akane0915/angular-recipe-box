2cf92d59719b05fb8ed1c8ab8cc5cf8eaa4b40d8
Asia Kane and Susha Dore <asia@>
2017-06-06 09:54:59 -0700

    add Recipe class listing recipes on index

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>Granny's Recipes</h1>
+  <div class="container">
+    <h1>Granny's Recipes</h1>
+    <ul>
+      <li *ngFor="let recipe of recipes">{{recipe.title}}</li>
+    </ul>
+  </div>
   `
 })
 
 export class AppComponent {
+  recipes: Recipe[] = [
+    new Recipe('Apple Pie', 'Apples, sugar, butter, cinnamon, flour, water', 'Prep apples, prep crust, bake'),
+    new Recipe('Liver and Onions', 'Liver, onions', 'cook them and then most likely toss it because it is inedible really'),
+  ];
 }
+
+export class Recipe {
+  constructor(public title: string, public ingredients: string, public directions: string) {}
+ }
