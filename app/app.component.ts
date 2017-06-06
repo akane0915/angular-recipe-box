8eb1a7f127948e1c23803fc08dcfcbdb97b5f43e
Asia Kane and Susha Dore <susha@>
2017-06-06 14:19:17 -0700

    add styling to make recipe titles appear to be links

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,13 +6,20 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Granny's Recipes</h1>
     <ul>
-      <li *ngFor="let recipe of recipes">{{recipe.title}} <button class="btn" (click)="editRecipe(recipe)">Edit Recipe</button></li>
+      <li *ngFor="let recipe of recipes" (click)="showRecipeDetails(recipe)"><span class="link">{{recipe.title}}</span><button (click)="editRecipe(recipe)">Edit Recipe</button>
+        <div *ngIf="recipeToBeShown == recipe">
+          <div *ngIf="recipeToBeShown">
+            <h3>{{recipeToBeShown.title}}</h3>
+            <p>Ingredients: {{recipeToBeShown.ingredients}}</p>
+            <p>Directions: {{recipeToBeShown.directions}}</p>
+            <br>
+          </div>
+        </div>
+      </li>
     </ul>
 
     <div *ngIf="selectedRecipe">
       <h3>{{selectedRecipe.title}}</h3>
-      <p>Ingredients: {{selectedRecipe.ingredients}}</p>
-      <p>Directions: {{selectedRecipe.directions}}</p>
       <br>
       <h4>Edit Recipe</h4>
       <label>Recipe Title:</label>
@@ -34,9 +41,14 @@ export class AppComponent {
     new Recipe('Plastic Noodles', 'Beef, rice noodles, onions, peppers, tofu, sauce', 'cook it in a wok')
   ];
 
-  selectedRecipe = null;
+  selectedRecipe: Recipe = null;
+  recipeToBeShown: Recipe = null;
 
-  editRecipe(clickedRecipe) {
+  showRecipeDetails(recipe: Recipe) {
+    this.recipeToBeShown = recipe;
+  }
+
+  editRecipe(clickedRecipe: Recipe) {
     this.selectedRecipe = clickedRecipe;
   }
 
